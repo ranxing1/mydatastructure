0172dad976f8d1dc19fbcc9c1095d3ffaf81c915
ranxing1 <91450289+ranxing1@users.noreply.github.com>
2022-11-03 03:05:27 +0800

    Add files via upload

---
+++ myqueue.h
@@ -0,0 +1,67 @@
+#include "mylist.h"
+#include <mutex>
+
+template<typename V>
+class Queue {
+public:
+	Queue();
+	~Queue();
+	V top();
+	bool pop();
+	bool push(V value);
+	int getSize();
+private:
+	List<V, V>* list;
+	int size;
+	std::mutex mtx;
+};
+
+template<typename V>
+Queue<V>::Queue() : size(0)
+{
+	list = new List<V, V>();
+}
+
+template<typename V>
+Queue<V>::~Queue()
+{
+	delete list;
+}
+
+template<typename V>
+V Queue<V>::top()
+{
+	return list->head->next->getValue();
+}
+
+template<typename V>
+bool Queue<V>::pop()
+{
+	bool flag = list->deleteNode(list->head->next->getKey());
+	if (flag)
+	{
+		mtx.lock();
+		size--;
+		mtx.unlock();
+	}
+	return flag;
+}
+
+template<typename V>
+bool Queue<V>::push(V value)
+{
+	bool flag = list->push_back(value, value);
+	if (flag)
+	{
+		mtx.lock();
+		size++;
+		mtx.unlock();
+	}
+	return flag;
+}
+
+template<typename V>
+int Queue<V>::getSize()
+{
+	return size;
+}
